c70bf97dcd035308cff90a7cf5f2d3e3ba529c75
kishorbinwade <kbinwade.1996@gmail.com>
2025-04-17 17:07:57 +0530

    url changes

--- resume_builder/resume_builder/urls.py
+++ resume_builder/resume_builder/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('create/', include('resumes.urls')),  # Use the resumes app URLs
+    path('', include('resumes.urls')),  # Use the resumes app URLs
 ]
 
 
